69e1b363775383f8266ea8754d0cd9294a989b0f
Nebby-12 <benvllcrt@gmail.com>
2022-07-08 21:48:12 +0800

    inayos yung Constants

--- src/main/java/constant.java
+++ src/main/java/constant.java
@@ -3,19 +3,35 @@ import java.util.Arrays;
 
 public class constant {
     static ArrayList<String> keywords = new ArrayList<>(
-            Arrays.asList("abstract", "assert", "boolean", "break", "byte", "case", "catch", "char",
-                    "class", "const", "continue", "default", "do", "double", "else", "enum", "extends",
-                    "final", "finally", "float", "for", "goto", "if", "implements", "import", "instanceof",
-                    "int", "interface", "long", "native", "new", "package", "private", "protected", "public",
-                    "return", "short", "static", "strictfp", "super", "switch", "synchronized", "this",
-                    "throw", "throws", "transient", "try", "void", "volatile", "while")
+            Arrays.asList("abstract", "class", "default", "enum", "extends", "implements",
+                    "instanceof", "interface", "native", "package", "public", "static",
+                    "strictfp", "super", "synchronized", "transient",  "void", "volatile")
     );
+
+    static ArrayList<String> semiKeywords = new ArrayList<>(
+            Arrays.asList("throw", "throws", "return", "break", "continue", "const", "import",
+                    "assert", "final", "new", "private", "protected")
+    );
+
+    static ArrayList<String> conditionals = new ArrayList<>(
+            Arrays.asList("catch", "try", "finally", "do", "while", "if", "else", "switch", "case", "for")
+    );
+
+    static ArrayList<String> dataTypes = new ArrayList<>(
+            Arrays.asList("this", "boolean", "byte", "int", "char", "long", "double", "float", "short", "String")
+    );
+
     static ArrayList <String> operators = new ArrayList<>(
             Arrays.asList("=", "+", "-", "/", "*")
     );
     static ArrayList <String> punctuators = new ArrayList<>(
-            Arrays.asList("!", "%", "^", "&", "(", ")", "{", "}", "|", "~",
-                    "[", "]", ";", ":", "<", ">", "?", ",", ".", "#")
+            Arrays.asList("!", "%", "^", "&",  "|", "~",
+                     ":", "<", ">", "?", ".", "#")
     );
+    static ArrayList<String> separators = new ArrayList<>(
+            Arrays.asList("{", "}", "[", "]", "(", ")", ",")
+    );
+    static String semicolon = new String(";");
+
 }
 
