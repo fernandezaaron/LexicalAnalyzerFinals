11d6618277f95501bdc77542e1a80bbf31643896
Nebby-12 <benvllcrt@gmail.com>
2022-07-08 18:15:13 +0800

    lexical solver

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,5 +1,34 @@
+import java.lang.*;
+
 public class main {
     public static void main(String[] args) {
         gui design = new gui();
     }
+
+
+    public static boolean isalnum(char val){
+        return (val >= 'a' && val <= 'z') || (val >= 'A' && val <= 'Z') || (val >= '0' && val <= '9');
+    }
+    public void lexicalSolver(char word[]){
+        int start = 0;
+        int end = 0;
+        int peek = 0;
+        String value = "";
+        for(int i = 0; i< word.length; i++){
+            start = i;
+            end = i;
+            if (isalnum(word[i])) {
+                peek = end + 1;
+                if (!isalnum(word[peek]) && (word[peek] != '\0')) {
+                    break;
+                }
+                else {
+                    end++;
+                }
+                value = word.substring();
+            }
+        }
+    }
+
+
 }
