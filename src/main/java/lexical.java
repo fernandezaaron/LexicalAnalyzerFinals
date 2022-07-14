c749092446a1701d562601c04fe91169e490281d
RjEsg <rjme101400@gmail.com>
2022-07-14 19:06:05 +0800

    semicolon shits

--- src/main/java/lexical.java
+++ src/main/java/lexical.java
@@ -11,7 +11,9 @@ public class lexical {
     private boolean multiFlag = false;
     private boolean singleFlag = false;
     private boolean functionFlag = false;
-    private boolean methodFlag =false;
+    private boolean methodFlag = false;
+    private boolean semicolonFlag = false;
+    private boolean endstatementFlag = false;
 
     // Constructor is used to save the code
     public lexical(String code){
@@ -33,35 +35,64 @@ public class lexical {
         int start = 0;
         int end = 0;
         int peek = 0;
+        int intStatement = 0;
         String codeinput = "";
         String value = "";
+        String stringStatement = "";
 
 
         for (int i = 0; i < this.code.length(); i++) {
             start = i;
             end = i;
             if((isalnum(String.valueOf(this.code.charAt(i))))){
-
                 while((end < this.code.length())){
                     peek = end + 1;
                     try {
-                        if (this.code.charAt(peek) == '\0' || this.code.charAt(peek) == '\n'  || this.code.charAt(peek) == ' ' || this.code.charAt(peek) == '\t') {
-                            end++;
-                            break;
-                        }
+//                        if (this.code.charAt(peek) == '\0' || this.code.charAt(peek) == '\n'  || this.code.charAt(peek) == ' ' || this.code.charAt(peek) == '\t') {
+//                            end++;
+//                            break;
+//                        }
+//                        if(this.code.charAt(peek) == '.'){
+//                            methodFlag = true;
+//                        }
+//                        if(varFlag && this.code.charAt(peek) == '('){
+//                            functionFlag = true;
+//                            varFlag = false;
+//                            break;
+//                        }
+//                        if(!isalnum(String.valueOf(this.code.charAt(peek)))){
+//                            break;
+//                        }
+//                        if(this.code.charAt(peek) == ';'){
+//                            output.add(new container(this.code.substring(start,peek+1), "n/a", "Statement"));
+//                            break;
+//                        }
+//                        else {
+//                            end++;
+//                        }
+
                         if(this.code.charAt(peek) == '.'){
                             methodFlag = true;
+                            break;
                         }
                         if(varFlag && this.code.charAt(peek) == '('){
                             functionFlag = true;
                             varFlag = false;
                             break;
                         }
+                        if (this.code.charAt(peek) == '\0' || this.code.charAt(peek) == '\n'  || this.code.charAt(peek) == ' ' || this.code.charAt(peek) == '\t') {
+                            end++;
+                            if (this.code.charAt(peek) == '\0' || this.code.charAt(peek) == '\n') {
+                                endstatementFlag = true;
+                            }
+                            break;
+                        }
                         if(!isalnum(String.valueOf(this.code.charAt(peek)))){
                             break;
                         }
                         if(this.code.charAt(peek) == ';'){
-                            output.add(new container(this.code.substring(start,peek+1), "n/a", "Statement"));
+                            endstatementFlag = true;
+                            semicolonFlag = false;
                             break;
                         }
                         else {
@@ -69,6 +100,7 @@ public class lexical {
                         }
                     }
                     catch (StringIndexOutOfBoundsException e) {
+                        endstatementFlag = true;
                         break;
                     }
                     i = end;
@@ -140,7 +172,6 @@ public class lexical {
                 // END OF READING IF COMMENTS
                 while ((end < this.code.length())){
                     peek = end + 1;
-
                         if (String.valueOf(this.code.charAt(end)).contains(" ")) {
                             break;
                         }
@@ -149,101 +180,103 @@ public class lexical {
                         }
                 }
             }
-                    codeinput = this.code.substring(start, peek);
-                    if(constant.keywords.contains(codeinput)) {
-                        varFlag = true;
-                        output.add(new container(codeinput,",","Keywords"));
-                    }
-                    else if(constant.semiKeywords.contains(codeinput)) {
-                        output.add(new container(codeinput,",","Keywords"));
-                    }
-                    else if(constant.conditionals.contains(codeinput)) {
-                        output.add(new container(codeinput,",","conditionals"));
-                    }
-                    //checks if it datatypes contain codeinput and activates varFlag since a data type will have a variable name afterwards
-                    else if(constant.dataTypes.contains(codeinput)) {
-                        output.add(new container(codeinput,",","dataTypes"));
-                        varFlag = true;
-                    }
-                    else if (constant.operators.contains(codeinput)) {
-                        output.add(new container(codeinput,",","operators"));
-                    }
-                    else if (constant.punctuators.contains(codeinput)) {
-                        output.add(new container(codeinput,",","punctuators"));
-                    }
-                    else if (constant.separators.contains(codeinput)) {
-                        if(codeinput.equals("(") || codeinput.equals("[")){
-                            varFlag = true;
-                        }
-                        output.add(new container(codeinput,",","separators"));
-                    }
-                    else if (constant.boolTypes.contains(codeinput)) {
-                        output.add(new container(codeinput, ",", "boolean"));
-                    }
-                    else if (doubleQuote != "\0") {
-                        output.add(new container(codeinput, "n/a", doubleQuote));
-                        doubleQuote = "\0";
-                    }
-                    else if (codeinput.matches(" ") || codeinput.matches("\n") || codeinput.matches("\t")) {
-                        continue;
-                    }
-                    else if(singleFlag){
-                        output.add(new container(codeinput, "n/a", "Single-Line Comment"));
-                        singleFlag = false;
-                    }
-                    else if(multiFlag){
-                        output.add(new container(this.code.substring(start,peek), "n/a", "Multi-Line Comment"));
-                        multiFlag = false;
-                    }
+            codeinput = this.code.substring(start, peek);
+            stringStatement += codeinput;
+            if(constant.keywords.contains(codeinput)) {
+                output.add(new container(codeinput,",","Keywords"));
+                varFlag = true;
+            }
+            else if(constant.semiKeywords.contains(codeinput)) {
+                output.add(new container(codeinput,",","Keywords"));
+            }
+            else if(constant.conditionals.contains(codeinput)) {
+                output.add(new container(codeinput,",","Conditionals"));
+            }
+            //checks if it datatypes contain codeinput and activates varFlag since a data type will have a variable name afterwards
+            else if(constant.dataTypes.contains(codeinput)) {
+                output.add(new container(codeinput,",","Data Types"));
+                semicolonFlag = true;
+                varFlag = true;
+            }
+            else if (constant.operators.contains(codeinput)) {
+                output.add(new container(codeinput,",","operators"));
+            }
+            else if (constant.punctuators.contains(codeinput)) {
+                output.add(new container(codeinput,",","punctuators"));
+            }
+            else if (constant.separators.contains(codeinput)) {
+                if(codeinput.equals("(") || codeinput.equals("[")){
+                    varFlag = true;
+                }
+                output.add(new container(codeinput,",","separators"));
+            }
+            else if (constant.boolTypes.contains(codeinput)) {
+                output.add(new container(codeinput, ",", "boolean"));
+            }
+            else if (doubleQuote != "\0") {
+                output.add(new container(codeinput, "n/a", doubleQuote));
+                doubleQuote = "\0";
+            }
+            else if (codeinput.matches(" ") || codeinput.matches("\n") || codeinput.matches("\t")) {
+                continue;
+            }
+            else if(singleFlag){
+                output.add(new container(codeinput, "n/a", "Single-Line Comment"));
+                singleFlag = false;
+            }
+            else if(multiFlag){
+                output.add(new container(this.code.substring(start,peek), "n/a", "Multi-Line Comment"));
+                multiFlag = false;
+            }
+            else if (semicolonFlag && !stringStatement.endsWith(";") && endstatementFlag) {
+                output.add(new container(codeinput, "Error", "Semicolon not found."));
+                semicolonFlag = false;
+                endstatementFlag = false;
+                stringStatement = "";
+            }
+            else if(variableContainer.contains(codeinput)){
+                variableContainer.add(codeinput);
+                output.add(new container(codeinput, "USED", "variable"));
+            }
+            //checks if variable is valid
+            else if(varFlag && Character.isDigit(codeinput.charAt(0)) || codeinput.charAt(0) == '_'){
+                output.add(new container(codeinput, ",", "Invalid Variable Name"));
+                varFlag = false;
+            }
+            //if variable is valid, it will set the value to not used and add the current codeinput to the variable arraylist
+            else if (varFlag){
+                variableContainer.add(codeinput);
+                if(!duplicate(variableContainer)) {
+                    output.add(new container(codeinput, "Declared", "variable"));
+                }
+                else {
+                    output.add(new container(codeinput, "Not Declared", "variable"));
+                    variableContainer.add(codeinput);
+                }
+                varFlag = false;
 
-                    else if(variableContainer.contains(codeinput)){
-                        variableContainer.add(codeinput);
-                        output.add(new container(codeinput, "USED", "variable"));
-                    }
-
-                    //checks if variable is valid
-                    else if(varFlag && Character.isDigit(codeinput.charAt(0)) || codeinput.charAt(0) == '_'){
-                        output.add(new container(codeinput, ",", "Invalid Variable Name"));
-                        varFlag = false;
-                    }
-
-                    //if variable is valid, it will set the value to not used and add the current codeinput to the variable arraylist
-                    else if (varFlag){
-
-                        variableContainer.add(codeinput);
-
-
-                        if(!duplicate(variableContainer)){
-                            output.add(new container(codeinput, "Declared", "variable"));
-                        }else{
-                            output.add(new container(codeinput, "Not Declared", "variable"));
-                            variableContainer.add(codeinput);
-                        }
-                        varFlag = false;
-
-                    }
-                    else if(functionFlag){
-                        output.add(new container(codeinput, "", "Function Name"));
-                        functionFlag = false;
-                    }
-                    else if(!variableContainer.contains(codeinput)  && !Character.isDigit(codeinput.charAt(0))){
-                        if(methodFlag){
-                            output.add(new container(codeinput, ",", "Method"));
-                            methodFlag = false;
-                        }
-//                        else{
-//                            output.add(new container(codeinput, "Error", "Cannot Resolve Symbol"));
-//                        }
-
-                    }
-                    else{
-                        output.add(new container(codeinput, ",", "identifier"));
-                    }
-                     //varFlag = false;
-                    // multiFlag = false;
-                    // singleFlag = false;
-                     functionFlag = false;
-                    // methodFlag =false;
+            }
+            else if(functionFlag) {
+                output.add(new container(codeinput, "", "Function Name"));
+                functionFlag = false;
+            }
+            else if(!variableContainer.contains(codeinput)  && !Character.isDigit(codeinput.charAt(0))){
+                if(methodFlag) {
+                    output.add(new container(codeinput, ",", "Method"));
+                    methodFlag = false;
+                }
+//                      else{
+//                          output.add(new container(codeinput, "Error", "Cannot Resolve Symbol"));
+//                      }
+            }
+            else{
+                output.add(new container(codeinput, ",", "identifier"));
+            }
+            //varFlag = false;
+            // multiFlag = false;
+            // singleFlag = false;
+            functionFlag = false;
+            // methodFlag =false;
             i = end;
         }
     }
